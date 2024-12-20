c69587d59ebdf05cb9956811ccb36efd23153e28
Andrea H <andreajhl29@gmail.com>
2024-12-20 13:20:18 -0300

    feat: :wrench: add initial config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Incluye todos los archivos relevantes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
